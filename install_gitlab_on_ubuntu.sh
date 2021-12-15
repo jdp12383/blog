5b3f11c6e0e99a56e1b3b685d5d252e46a5b28e5
Jignesh Parikh <jignesh.parikh.12383@gmail.com>
2021-12-14 21:11:26 -0600

    how to change root user password

--- install_gitlab_on_ubuntu.sh
+++ install_gitlab_on_ubuntu.sh
@@ -9,3 +9,16 @@ $ sudo -e /etc/gitlab/gitlab.rb
 // find external_url and change the port 80 -> 88
 $ sudo gitlab-ctl reconfigure
 $ sudo gitlab-ctl start/stop/restart
+
+// change root user password
+$ sudo gitlab-rails console
+irb(main):001:0> user = User.find_by_username 'root'
+=> #<User id:1 @root>
+irb(main):002:0> user.password = 'password'
+=> "password"
+irb(main):003:0> user.password_confirmation = 'password'
+=> "password"
+irb(main):004:0> user.save!
+=> true
+irb(main):005:0> exit
+
